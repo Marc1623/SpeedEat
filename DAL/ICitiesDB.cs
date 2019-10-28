f305cd5ad32ab556ec9f28fde7445e7af2712e55
mathieu <mathieu.kohl@students.hevs.ch>
2019-10-28 15:10:31 +0100

    création DTO

---
+++ DAL/ICitiesDB.cs
@@ -0,0 +1,20 @@
+﻿using DataTransferObject;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DAL
+{
+    public interface ICitiesDB
+    {
+        IConfiguration Configuration { get; }
+        List<C> GetAllHotels();
+        CitiesDB GetHotel(int id);
+        CitiesDB AddHotel(CitiesDB cities );
+        CitiesDB UpdateHotel(CitiesDB cities);
+        int DeleteHotel(int id);
+    }
+
+}
