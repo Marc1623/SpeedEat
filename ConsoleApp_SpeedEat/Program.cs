24eb023272ef397eb56ed854ecfd68030a6f69cb
mathieu <mathieu.kohl@students.hevs.ch>
2019-10-28 09:58:40 +0100

    ajout du DAL

--- ConsoleApp_SpeedEat/Program.cs
+++ ConsoleApp_SpeedEat/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp_SpeedEat
 {
     class Program
     {
+
+            public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+           .Build();
+
         static void Main(string[] args)
         {
             Console.WriteLine("nike ta mere!");
