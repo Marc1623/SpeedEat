97bec5cf4c996c20964ee0437ced9d8177981571
mathieu <mathieu.kohl@students.hevs.ch>
2019-10-29 18:17:13 +0100

    dto delivery

---
+++ DTO/Delivery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class Delivery
+    {
+        public int IdDelivery { get; set; }
+
+        public string Start_Time { get; set; }
+
+        public string End_Time { get; set; }
+
+        public string Created_At { get; set; }
+
+        public int Fk_Id_Deliverer { get; set; }
+
+        public override string ToString()
+        {
+            return $"{IdDelivery}|{Start_Time}|{End_Time}|{Created_At}|{Fk_Id_Deliverer}";
+        }
+    }
+}
